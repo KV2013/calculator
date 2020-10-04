a875b417aff6de0a608547319e4f7d7d7cff0e90
Kilikaev Victor <kv2013@gmail.com>
2020-10-04 14:27:19 +0500

    Настройка окружения

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin({}),
+  ],
+};
