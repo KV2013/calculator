45c3c96eb088336b47539b40f92ab4588b42a0f2
Kilikaev Victor <kv2013@gmail.com>
2020-10-04 21:28:13 +0500

    WIP Макет
    Размер шрифта кнопок

--- src/js/App.js
+++ src/js/App.js
@@ -12,96 +12,96 @@ function App() {
       <div id="buttons" className="grid grid-cols-4 grid-rows-5 flex-grow">
         <ul className="grid grid-rows-1 grid-cols-3 text-white col-start-1 col-end-4 gap-y-4">
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-400 text-black">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-400 text-black">
               AC
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-400 text-black">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-400 text-black">
               +/-
             </div>
           </li>
         </ul>
         <ul className="grid grid-cols-3 row-start-2 row-end-6 col-start-1 col-end-4 gap-y-4 text-white ">
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               1
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               2
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               3
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               4
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               5
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               6
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               7
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               8
             </div>
           </li>
           <li className="flex justify-center items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               9
             </div>
           </li>
           <li className="flex justify-center items-center col-start-1 col-end-3 px-2">
-            <div className="flex justify-start items-center pl-8 w-full h-16 rounded-full bg-gray-900">
+            <div className="flex justify-start items-center pl-8 w-full h-16 text-4xl rounded-full bg-gray-900">
               0
             </div>
           </li>
           <li className="flex justify-center items-center col-start-3 col-end-4 ">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-gray-900">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900">
               ,
             </div>
           </li>
         </ul>
         <ul className="grid grid-cols-1 grid-rows-5 row-start-1 row-end-6 gap-y-4 text-white ">
           <li className="flex justify-end items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-orange-400">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400">
               &#247;
             </div>
           </li>
           <li className="flex justify-end items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-orange-400">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400">
               &#10005;
             </div>
           </li>
           <li className="flex justify-end items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-orange-400">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400">
               -
             </div>
           </li>
           <li className="flex justify-end items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-orange-400">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400">
               +
             </div>
           </li>
           <li className="flex justify-end items-center">
-            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-orange-400">
+            <div className="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400">
               =
             </div>
           </li>
