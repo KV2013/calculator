d059de7ea5bf854bc90603fe541fb92c9c6da525
Kilikaev Victor <kv2013@gmail.com>
2020-10-05 09:52:42 +0500

    Добавил индикацию hover active и focus состояний для кнопок

--- src/js/App.js
+++ src/js/App.js
@@ -14,98 +14,98 @@ function App() {
         <ul className="grid grid-rows-1 grid-cols-3 text-white col-start-1 col-end-4 gap-y-4">
           <li className="flex justify-center items-center">
             <Btn
-              cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-400 text-black hover:bg-gray-300"
+              cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-gray-400 text-black hover:bg-gray-300 active:bg-gray-500"
               clickHandler={() => console.log("AC")}
             >
               AC
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-400 text-blackk hover:bg-gray-300">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-gray-400 text-black hover:bg-gray-300 active:bg-gray-500">
               +/-
             </Btn>
           </li>
         </ul>
         <ul className="grid grid-cols-3 row-start-2 row-end-6 col-start-1 col-end-4 gap-y-4 text-white ">
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               7
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               8
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               9
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               4
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               5
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               6
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               1
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               2
             </Btn>
           </li>
           <li className="flex justify-center items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               3
             </Btn>
           </li>
           <li className="flex justify-center items-center col-start-1 col-end-3 px-2">
-            <Btn cname="flex justify-start items-center pl-8 w-full h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-start items-center pl-8 w-full h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               0
             </Btn>
           </li>
           <li className="flex justify-center items-center col-start-3 col-end-4 ">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-900 hover:bg-gray-800">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-gray-800 focus:outline-none focus:shadow-outline hover:bg-gray-700 active:bg-gray-900">
               ,
             </Btn>
           </li>
         </ul>
         <ul className="grid grid-cols-1 grid-rows-5 row-start-1 row-end-6 gap-y-4 text-white ">
           <li className="flex justify-end items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400 hover:bg-orange-300">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-orange-400 hover:bg-orange-300 active:bg-orange-500">
               &#247;
             </Btn>
           </li>
           <li className="flex justify-end items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400 hover:bg-orange-300">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-orange-400 hover:bg-orange-300 active:bg-orange-500">
               &#10005;
             </Btn>
           </li>
           <li className="flex justify-end items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400 hover:bg-orange-300">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-orange-400 hover:bg-orange-300 active:bg-orange-500">
               -
             </Btn>
           </li>
           <li className="flex justify-end items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400 hover:bg-orange-300">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-orange-400 hover:bg-orange-300 active:bg-orange-500">
               +
             </Btn>
           </li>
           <li className="flex justify-end items-center">
-            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full bg-orange-400 hover:bg-orange-300">
+            <Btn cname="flex justify-center items-center w-16 h-16 text-4xl rounded-full focus:outline-none focus:shadow-outline bg-orange-400 hover:bg-orange-300 active:bg-orange-500">
               =
             </Btn>
           </li>
