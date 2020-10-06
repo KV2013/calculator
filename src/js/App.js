1d3b2f91d296c0747b05d6339bb1ec2588b65530
Kilikaev Victor <kv2013@gmail.com>
2020-10-06 15:51:33 +0500

    splits expression into two lines when the symbol limit is exceeded

--- src/js/App.js
+++ src/js/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import ExpressionHistory from "./components/ExpressionHistory";
 import Expression from "./components/Expression";
 import ButtonsGrid from "./components/ButtonsGrid";
+import { EXPRESSION_SYMBOLS_PER_LINE } from "./constants";
 
 function App() {
   return (
     <div className="flex flex-col bg-black w-full h-full p-6 lg:h-auto lg:max-w-sm">
       <ExpressionHistory />
-      <Expression />
+      <Expression symbolsPerLine={EXPRESSION_SYMBOLS_PER_LINE} />
       <ButtonsGrid />
     </div>
   );
--- src/js/components/Expression.js
+++ src/js/components/Expression.js
@@ -2,21 +2,34 @@ import React from "react";
 import { connect } from "react-redux";
 import { parseExpressionStack } from "../modules/expressionStack";
 
-const Expression = ({ expressionStack }) => {
+const Expression = ({ symbolsPerLine, expressionStack }) => {
   const expressionString = parseExpressionStack(expressionStack);
+  let expStringArr = [expressionString];
+  if (expressionString.length > symbolsPerLine) {
+    expStringArr = [
+      expressionString.slice(0, expressionString.length - symbolsPerLine),
+      expressionString.slice(-symbolsPerLine),
+    ];
+  }
+  console.log("EXPRESSION COMPONENT", { expStringArr });
 
   return (
     <div
       id="calculation"
-      className="h-32 flex flex-col pb-4
-     text-white text-right"
+      className="h-32 flex flex-col pb-4 text-white text-right"
     >
-      {/* <div className="flex-none flex items-end justify-end text-4xl leading-none">
-        <span>{expressionString}</span>
-      </div> */}
-      <div className="flex-auto flex items-end justify-end text-6xl leading-none">
-        <span>{expressionString}</span>
-      </div>
+      {expStringArr.map((exp, index, arr) => {
+        let fontClass = " text-6xl ";
+        if (arr.length > 1 && index < arr.length - 1) {
+          fontClass = " text-4xl ";
+        }
+        const className = `flex-auto flex items-end justify-end leading-none overflow-hidden ${fontClass}`;
+        return (
+          <div key={index} className={className}>
+            <span>{exp}</span>
+          </div>
+        );
+      })}
     </div>
   );
 };
