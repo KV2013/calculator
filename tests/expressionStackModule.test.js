0942f217869ddbc48c25dc8865c36292681a7953
Kilikaev Victor <kv2013@gmail.com>
2020-10-07 15:24:21 +0500

    Adds expressionStack module unit tests

---
+++ tests/expressionStackModule.test.js
@@ -0,0 +1,146 @@
+import * as expressionStack from "../src/js/modules/expressionStack";
+import * as constants from "../src/js/constants";
+
+describe("appendInput", () => {
+  test("2+2", () => {
+    let stack = [];
+    stack = expressionStack.appendInput(stack, "2", false);
+    stack = expressionStack.appendInput(
+      stack,
+      constants.CALC_OPERATION_PLUS,
+      false
+    );
+    stack = expressionStack.appendInput(stack, "2", false);
+
+    expect(stack).toStrictEqual([
+      JSON.stringify({ value: "2", isNegative: false, isCalculated: false }),
+      constants.CALC_OPERATION_PLUS,
+      JSON.stringify({ value: "2", isNegative: false, isCalculated: false }),
+    ]);
+  });
+
+  test("5,5*2", () => {
+    let stack = [];
+    stack = expressionStack.appendInput(stack, "5", false);
+    stack = expressionStack.appendInput(stack, constants.DECIMAL_SIGN, false);
+    stack = expressionStack.appendInput(stack, "5", false);
+    stack = expressionStack.appendInput(
+      stack,
+      constants.CALC_OPERATION_MULTIPLY,
+      false
+    );
+    stack = expressionStack.appendInput(stack, "2", false);
+
+    expect(stack).toStrictEqual([
+      JSON.stringify({ value: "5.5", isNegative: false, isCalculated: false }),
+      constants.CALC_OPERATION_MULTIPLY,
+      JSON.stringify({ value: "2", isNegative: false, isCalculated: false }),
+    ]);
+  });
+
+  test("15 - + * / 3", () => {
+    let stack = [];
+    const inputStream = [
+      "1",
+      "5",
+      constants.CALC_OPERATION_MINUS,
+      constants.CALC_OPERATION_PLUS,
+      constants.CALC_OPERATION_MULTIPLY,
+      constants.CALC_OPERATION_DIVISION,
+      "3",
+    ];
+    inputStream.forEach((input, index) => {
+      stack = expressionStack.appendInput(stack, input, false);
+    });
+
+    expect(stack).toStrictEqual([
+      JSON.stringify({ value: "15", isNegative: false, isCalculated: false }),
+      constants.CALC_OPERATION_DIVISION,
+      JSON.stringify({ value: "3", isNegative: false, isCalculated: false }),
+    ]);
+  });
+
+  test("every number", () => {
+    let stack = [];
+    const inputStream = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    inputStream.forEach((input) => {
+      let newStack = expressionStack.appendInput(stack, input, false);
+      expect(newStack.length).toBe(1);
+      expect(newStack).toStrictEqual([
+        JSON.stringify({
+          value: input,
+          isNegative: false,
+          isCalculated: false,
+        }),
+      ]);
+
+      newStack = expressionStack.appendInput(stack, input, true);
+      expect(newStack.length).toBe(1);
+      expect(newStack).toStrictEqual([
+        JSON.stringify({
+          value: input,
+          isNegative: true,
+          isCalculated: false,
+        }),
+      ]);
+    });
+  });
+
+  test("every operation", () => {
+    let stack = [];
+    const inputStream = [
+      constants.CALC_OPERATION_MINUS,
+      constants.CALC_OPERATION_PLUS,
+      constants.CALC_OPERATION_MULTIPLY,
+      constants.CALC_OPERATION_DIVISION,
+    ];
+    inputStream.forEach((input) => {
+      let stack = [];
+
+      stack = expressionStack.appendInput(stack, "1", false);
+      stack = expressionStack.appendInput(stack, input, false);
+      expect(stack.length).toBe(2);
+      expect(stack).toStrictEqual([
+        JSON.stringify({
+          value: "1",
+          isNegative: false,
+          isCalculated: false,
+        }),
+        input,
+      ]);
+
+      stack = [];
+      stack = expressionStack.appendInput(stack, "1", true);
+      stack = expressionStack.appendInput(stack, input, true);
+      expect(stack.length).toBe(2);
+      expect(stack).toStrictEqual([
+        JSON.stringify({
+          value: "1",
+          isNegative: true,
+          isCalculated: false,
+        }),
+        input,
+      ]);
+    });
+  });
+});
+
+describe("parseExpressionStack", () => {
+  test("parse 01.001", () => {
+    let stack = [];
+    let inputStream = ["0", "1", constants.DECIMAL_SIGN, "0", "0", "1"];
+    const isNegative = false;
+    const isCalculated = false;
+
+    inputStream.forEach(
+      (input) => (stack = expressionStack.appendInput(stack, input, isNegative))
+    );
+    expect(stack.length).toBe(1);
+
+    const parsedExpression = expressionStack.parseExpressionStack(stack);
+    expect(parsedExpression).toBe("1" + constants.DECIMAL_SIGN + "001");
+  });
+});
+
+// describe("applyNegativeNumberMode", () => {});
+// describe("calculateExpression", () => {});
